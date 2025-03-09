f0f845ab628deb87032c6cbec43675ed00cb99f9
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2025-03-09 20:59:31 +0800

    配置文件同时支持JSON和YAML

--- nyamysql/go.mod
+++ nyamysql/go.mod
@@ -4,10 +4,5 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/tidwall/gjson v1.14.3
-)
-
-require (
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1
 )
--- nyamysql/nyamysqlmain.go
+++ nyamysql/nyamysqlmain.go
@@ -7,8 +7,18 @@ import (
 	"fmt"
 	"log"
 	"regexp"
+
+	"gopkg.in/yaml.v3"
 )
 
+// MySQLDBConfig 結構體用於配置MySQL資料庫的連線引數。
+// 該結構體包含以下欄位：
+// - User: MySQL使用者名稱。
+// - Password: MySQL密碼。
+// - Address: MySQL伺服器地址。
+// - Port: MySQL伺服器埠。
+// - DbName: 資料庫名稱。
+// - MaxLimit: 資料的最大限制。
 type MySQLDBConfig struct {
 	User     string `json:"mysql_user" yaml:"mysql_user"`
 	Password string `json:"mysql_pwd" yaml:"mysql_pwd"`
@@ -18,7 +28,16 @@ type MySQLDBConfig struct {
 	MaxLimit string `json:"mysql_limit" yaml:"mysql_limit"`
 }
 
+// NyaMySQL 是一個類型別名，指向 NyaMySQLT 結構體。
+// 該類型別名用於簡化程式碼中的型別引用。
 type NyaMySQL NyaMySQLT
+
+// NyaMySQLT 結構體用於封裝MySQL資料庫連線及其相關配置。
+// 該結構體包含以下欄位：
+// - db: 指向MySQL資料庫連線的指標。
+// - limit: 資料庫連線的最大限制。
+// - err: 用於儲存資料庫操作中的錯誤資訊。
+// - debug: 用於除錯的日誌記錄器。
 type NyaMySQLT struct {
 	db    *sql.DB
 	limit string
@@ -26,33 +45,62 @@ type NyaMySQLT struct {
 	debug *log.Logger
 }
 
-// New: 建立新的 NyaMySQL 例項
+// New 函式用於根據傳入的配置字串建立一個新的 NyaMySQL 例項。
+// 該函式嘗試解析配置字串為 JSON 或 YAML 格式，並根據解析結果初始化 MySQL 配置。
+// 如果解析成功，則呼叫 NewC 函式建立並返回 NyaMySQL 例項；如果解析失敗，則返回一個包含錯誤的 NyaMySQL 例項。
 //
-//	`configJsonString` string 配置 JSON 字串
-//	從配置 JSON 檔案中取出的本模組所需的配置段落 JSON 字串
-//	示例配置數值參考 config.template.json
-//	本模組所需配置項: mysql_addr, mysql_port, mysql_db, mysql_user, mysql_pwd
-//	return *NyaMySQL 新的 NyaMySQL 例項
-//	下一步使用 `Error()` 或 `ErrorString()` 檢查是否有錯誤
-func New(configJsonString string, Debug *log.Logger) *NyaMySQL {
+// 引數:
+//   - configString: 包含 MySQL 配置資訊的字串，可以是 JSON 或 YAML 格式。
+//   - Debug: 用於記錄除錯資訊的日誌記錄器。
+//
+// 返回值:
+//   - *NyaMySQL: 返回一個 NyaMySQL 例項指標，如果配置解析失敗，例項中將包含錯誤資訊。
+func New(configString string, Debug *log.Logger) *NyaMySQL {
 	var mySQLConfig MySQLDBConfig
-	err := json.Unmarshal([]byte(configJsonString), &mySQLConfig)
-	if err != nil {
-		return &NyaMySQL{err: err}
+	var err error = nil
+
+	// 嘗試將配置字串解析為 JSON 格式
+	if err := json.Unmarshal([]byte(configString), &mySQLConfig); err == nil {
+		return NewC(mySQLConfig, Debug)
 	}
-	return NewC(mySQLConfig, Debug)
+
+	// 如果 JSON 解析失敗，嘗試將配置字串解析為 YAML 格式
+	if err := yaml.Unmarshal([]byte(configString), &mySQLConfig); err == nil {
+		return NewC(mySQLConfig, Debug)
+	}
+
+	// 如果 JSON 和 YAML 解析均失敗，返回一個包含錯誤的 NyaMySQL 例項
+	return &NyaMySQL{err: err}
 }
 
-// NewC: 同上, `configJsonString` 改為 `mySQLConfig` 以支援直接配置輸入
+// NewC 建立一個新的 NyaMySQL 例項，用於管理與 MySQL 資料庫的連線。
+// 該函式接受 MySQL 資料庫的配置資訊和一個除錯日誌記錄器，並返回一個初始化後的 NyaMySQL 物件。
+// 如果連線或 ping 資料庫時發生錯誤，返回的 NyaMySQL 物件將包含該錯誤資訊。
+//
+// 引數:
+//   - mySQLConfig: MySQLDBConfig 結構體，包含 MySQL 資料庫的連線配置資訊，如使用者名稱、密碼、地址、埠和資料庫名稱。
+//   - Debug: *log.Logger 型別，用於記錄除錯資訊的日誌記錄器。可以為 nil，表示不記錄除錯資訊。
+//
+// 返回值:
+//   - *NyaMySQL: 返回一個指向 NyaMySQL 結構體的指標，該結構體包含資料庫連線、最大連線限制和除錯日誌記錄器。
 func NewC(mySQLConfig MySQLDBConfig, Debug *log.Logger) *NyaMySQL {
+	// 根據 MySQL 配置資訊生成連線字串
 	var sqlsetting string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", mySQLConfig.User, mySQLConfig.Password, mySQLConfig.Address, mySQLConfig.Port, mySQLConfig.DbName)
+
+	// 使用生成的連線字串開啟資料庫連線
 	sqldb, err := sql.Open("mysql", sqlsetting)
 	if err != nil {
+		// 如果連線失敗，返回包含錯誤資訊的 NyaMySQL 物件
 		return &NyaMySQL{err: err}
 	}
+
+	// 嘗試 ping 資料庫以驗證連線是否成功
 	if err := sqldb.Ping(); err != nil {
+		// 如果 ping 失敗，返回包含錯誤資訊的 NyaMySQL 物件
 		return &NyaMySQL{err: err}
 	}
+
+	// 返回成功初始化的 NyaMySQL 物件，包含資料庫連線、最大連線限制和除錯日誌記錄器
 	return &NyaMySQL{
 		db:    sqldb,
 		limit: mySQLConfig.MaxLimit,
@@ -60,66 +108,93 @@ func NewC(mySQLConfig MySQLDBConfig, Debug *log.Logger) *NyaMySQL {
 	}
 }
 
-// SqlExec: 執行 SQL 語句
+// SqlExec 執行給定的SQL命令，並返回受影響行的最後插入ID。
+// 如果執行過程中發生錯誤，返回-1，並將錯誤資訊儲存在p.err中。
 //
-//	請先對要執行的語句進行安全檢查。建議使用 nyasql 生成 SQL 語句
-//	`sqlCmd` string 要執行的 SQL 語句
-//	return   int64  如果是插入操作返回 -1 表示失敗
+// 引數:
+//   - sqlCmd: 要執行的SQL命令字串。
+//
+// 返回值:
+//   - int64: 成功執行時返回最後插入的ID，失敗時返回-1。
 func (p *NyaMySQL) SqlExec(sqlCmd string) int64 {
+	// 執行SQL命令
 	result, err := p.db.Exec(sqlCmd)
 	p.err = err
+
+	// 如果執行過程中發生錯誤，返回-1
 	if err != nil {
 		return -1
 	}
+
+	// 獲取最後插入的ID並返回
 	id, _ := result.LastInsertId()
 	return id
 }
 
-// Error: 獲取上一次操作時可能產生的錯誤
+// Error 返回 NyaMySQL 例項中儲存的上一次操作產生的錯誤。
+// 該函式通常用於檢查在執行資料庫操作時是否發生了錯誤。
 //
-//	return error 如果有錯誤，返回錯誤物件，如果沒有錯誤返回 nil
+// 返回值:
+//   - error: 返回上一次操作中儲存的錯誤物件。如果沒有錯誤發生，則返回 nil。
 func (p *NyaMySQL) Error() error {
 	return p.err
 }
 
-// ErrorString: 獲取上一次操作時可能產生的錯誤資訊字串
+// ErrorString 返回與 NyaMySQL 例項關聯的錯誤資訊字串。
+// 如果沒有錯誤，則返回空字串。
 //
-//	return string 如果有錯誤，返回錯誤描述字串，如果沒有錯誤返回空字串
+// 返回值:
+//   - string: 如果存在錯誤，返回錯誤描述字串；否則返回空字串。
 func (p *NyaMySQL) ErrorString() string {
 	if p.err == nil {
 		return ""
 	}
+	// 返回底層錯誤物件的錯誤資訊。
 	return p.err.Error()
 }
 
-// Close: 斷開與資料庫的連線
+// Close 關閉與 NyaMySQL 例項關聯的資料庫連線。
+// 如果資料庫連線已經關閉或未初始化，則此函式不會執行任何操作。
+// 該方法確保在關閉連線後，將內部資料庫連線指標設定為 nil，以避免重複關閉或空指標引用。
 func (p *NyaMySQL) Close() {
+	// 檢查資料庫連線是否已初始化
 	if p.db != nil {
+		// 關閉資料庫連線
 		p.db.Close()
+		// 將資料庫連線指標置為 nil，防止重複關閉
 		p.db = nil
 	}
 }
 
-// 正则过滤sql注入的方法
+// SQLScurity 函式用於檢測輸入字串中是否包含常見的SQL注入攻擊模式。
+// 該函式透過正則表示式匹配來識別潛在的SQL注入關鍵字和模式。
 //
-//	text	string		要匹配的语句
-//	key		string		如果出错需要返回的 前端传入的key，例：username,password
-//	Error	*log.Logger	指定log对象，没有填写nil
-//	t		string		token或语言代码
-//	return	int			""：正常；其他：错误返回信息或错误代码
+// 引數:
+//   - text: 需要檢測的字串。
+//
+// 返回值:
+//   - 如果檢測到SQL注入模式，返回字串 "21001"；
+//   - 如果未檢測到SQL注入模式，返回空字串；
+//   - 如果正則表示式編譯失敗，返回錯誤資訊。
 func SQLScurity(text string) string {
-	//过滤 ‘
-	//ORACLE 注解 --  /**/
-	//关键字过滤 update ,delete
-	// 正则的字符串, 不能用 " " 因为" "里面的内容会转义
-	str := `(?:')|(?:--)|(/\\*(?:.|[\\n\\r])*?\\*/)|(\b(select|update|and|or|delete|insert|trancate|char|chr|into|substr|ascii|declare|exec|count|master|into|drop|execute)\b)`
+	// 定義正則表示式模式，用於匹配常見的SQL注入關鍵字和模式
+	// str := `(?:')|(?:--)|(/\\*(?:.|[\\n\\r])*?\\*/)|(\b(select|update|and|or|delete|insert|trancate|char|chr|into|substr|ascii|declare|exec|count|master|into|drop|execute)\b)`
+	str := `(?:')|(?:--)|(?:#)|(/\\*(?:.|[\\n\\r])*?\\*/)|(\b(select|update|and|or|delete|insert|truncate|char|chr|into|substr|ascii|declare|exec|count|master|into|drop|execute|union|join|having|group by|order by|case|when|then|else|cast|convert|nullif|coalesce|xp_cmdshell|sp_executesql)\b)|(\+|\|\|)|(0x[0-9a-fA-F]+)|(%[0-9a-fA-F]{2})|(1=1|1=0)|(' OR '1'='1|' AND '1'='1)|(; DROP TABLE|; SHUTDOWN)`
 	reint := ""
+
+	// 編譯正則表示式
 	re, err := regexp.Compile(str)
 	if err != nil {
+		// 如果編譯失敗，返回錯誤資訊
 		return err.Error()
 	}
+
+	// 檢測輸入字串是否匹配正則表示式
 	if re.MatchString(text) {
+		// 如果匹配，返回 "21001" 表示檢測到SQL注入模式
 		reint = "21001"
 	}
+
+	// 返回檢測結果
 	return reint
 }
