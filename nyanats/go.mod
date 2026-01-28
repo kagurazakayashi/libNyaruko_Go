f75c6cdf7ae24e947b3cb48f7aeeec26a238f59b
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2026-01-28 20:33:22 +0800

    NATS:连接和关闭连接

---
+++ nyanats/go.mod
@@ -0,0 +1,17 @@
+module github.com/kagurazakayashi/libNyaruko_Go/nyanats
+
+go 1.24.4
+
+require (
+	github.com/nats-io/nats.go v1.49.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.18.2 // indirect
+	github.com/nats-io/nkeys v0.4.12 // indirect
+	github.com/nats-io/nuid v1.0.1 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+)
---
+++ nyanats/nyanats.go
@@ -0,0 +1,85 @@
+package nyanats
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	"gopkg.in/yaml.v3"
+)
+
+type NATSConfig struct {
+	NatsServer   string `json:"nats_server" yaml:"nats_server"`
+	NatsUser     string `json:"nats_user" yaml:"nats_user"`
+	NatsPassword string `json:"nats_password" yaml:"nats_password"`
+}
+
+type NyaNATS struct {
+	err      error
+	natsConn *nats.Conn
+	debug    *log.Logger
+}
+
+func New(configString string, debug *log.Logger) *NyaNATS {
+	var natsConfig NATSConfig
+
+	if err := json.Unmarshal([]byte(configString), &natsConfig); err == nil {
+		return NewC(natsConfig, debug)
+	}
+
+	if err := yaml.Unmarshal([]byte(configString), &natsConfig); err == nil {
+		return NewC(natsConfig, debug)
+	}
+	return &NyaNATS{err: fmt.Errorf("failed to parse config"), debug: debug}
+}
+
+func NewC(natsConfig NATSConfig, debug *log.Logger) *NyaNATS {
+
+	url := fmt.Sprintf("%s/%s:%s@%s", "nats:/", natsConfig.NatsUser, natsConfig.NatsPassword, natsConfig.NatsServer)
+
+	opts := []nats.Option{
+		nats.Name("NyaNATS_Client"),
+		nats.MaxReconnects(5),
+		nats.ReconnectWait(2 * time.Second),
+		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+			if debug != nil {
+				debug.Printf("NATS Disconnected: %v", err)
+			}
+		}),
+	}
+
+	nc, err := nats.Connect(url, opts...)
+	if err != nil {
+		return &NyaNATS{err: err, debug: debug}
+	}
+
+	return &NyaNATS{
+		err:      nil,
+		natsConn: nc,
+		debug:    debug,
+	}
+}
+
+func (p *NyaNATS) Subscribe(theme string, callback func(m string) string) error {
+	return p.err
+}
+
+func (p *NyaNATS) Publish(theme string, message string) error {
+	return p.err
+}
+
+func (p *NyaNATS) Request(theme string, message string, timeout time.Duration) (string, error) {
+	return "", p.err
+}
+
+func (p *NyaNATS) Close() {
+	if p.natsConn != nil {
+		p.natsConn.Close()
+	}
+}
+
+func (p *NyaNATS) Error() error {
+	return p.err
+}
