8834812f1bf5a66188180745cdb78c8d206335b8
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2026-02-01 20:37:16 +0800

    NATS:提供各种默认设置

--- nyanats/nyanats.go
+++ nyanats/nyanats.go
@@ -6,25 +6,32 @@ import (
 	"log"
 	"time"
 
+	"github.com/google/uuid" // 需要运行 go get github.com/google/uuid
 	"github.com/nats-io/nats.go"
 	"gopkg.in/yaml.v3"
 )
 
 type NATSConfig struct {
-	NatsServer   string `json:"nats_server" yaml:"nats_server"`
-	NatsUser     string `json:"nats_user" yaml:"nats_user"`
-	NatsPassword string `json:"nats_password" yaml:"nats_password"`
-
+	NatsServer     string `json:"nats_server" yaml:"nats_server"`
+	NatsUser       string `json:"nats_user" yaml:"nats_user"`
+	NatsPassword   string `json:"nats_password" yaml:"nats_password"`
 	ClientName     string `json:"client_name" yaml:"client_name"`
 	MaxReconnects  int    `json:"max_reconnects" yaml:"max_reconnects"`
 	ReconnectWait  int    `json:"reconnect_wait" yaml:"reconnect_wait"`
 	ConnectTimeout int    `json:"connect_timeout" yaml:"connect_timeout"`
 }
 
+// setDefaults 填充默认值
 func (c *NATSConfig) setDefaults() {
-	if c.ClientName == "" {
-		c.ClientName = "NyaNATS_Client"
+	if c.NatsServer == "" {
+		c.NatsServer = "127.0.0.1:4222"
 	}
+
+	// 如果未指定名称，生成一个类似 "NyaNATS-550e8400-e29b..." 的唯一标识
+	if c.ClientName == "" {
+		c.ClientName = fmt.Sprintf("NyaNATS-%s", uuid.NewString())
+	}
+
 	if c.MaxReconnects == 0 {
 		c.MaxReconnects = 5
 	}
@@ -56,10 +63,16 @@ func New(configString string, debug *log.Logger) *NyaNATS {
 }
 
 func NewC(config NATSConfig, debug *log.Logger) *NyaNATS {
-
 	config.setDefaults()
 
-	url := fmt.Sprintf("nats://%s:%s@%s", config.NatsUser, config.NatsPassword, config.NatsServer)
+	var url string
+	if config.NatsUser != "" {
+
+		url = fmt.Sprintf("nats://%s:%s@%s", config.NatsUser, config.NatsPassword, config.NatsServer)
+	} else {
+
+		url = fmt.Sprintf("nats://%s", config.NatsServer)
+	}
 
 	opts := []nats.Option{
 		nats.Name(config.ClientName),
@@ -67,15 +80,14 @@ func NewC(config NATSConfig, debug *log.Logger) *NyaNATS {
 		nats.ReconnectWait(time.Duration(config.ReconnectWait) * time.Second),
 		nats.Timeout(time.Duration(config.ConnectTimeout) * time.Second),
 
-		nats.ErrorHandler(func(nc *nats.Conn, s *nats.Subscription, err error) {
-			if debug != nil {
-				debug.Printf("NATS Error in [%s]: %v", s.Subject, err)
-			}
-		}),
-
 		nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
 			if debug != nil {
-				debug.Printf("NATS Disconnected! Reason: %v", err)
+				debug.Printf("NyaNATS: 链接断开 - %v", err)
+			}
+		}),
+		nats.ReconnectHandler(func(nc *nats.Conn) {
+			if debug != nil {
+				debug.Printf("NyaNATS: 已重新连接至 %v", nc.ConnectedUrl())
 			}
 		}),
 	}
