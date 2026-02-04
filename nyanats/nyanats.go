76d801fc446ae55a5f8fe795c1502d5db761891c
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2026-02-04 20:57:42 +0800

    NATS:不同主题支持不同密钥

--- nyanats/nyanats.go
+++ nyanats/nyanats.go
@@ -23,8 +23,10 @@ type NATSConfig struct {
 	MaxReconnects  int    `json:"max_reconnects" yaml:"max_reconnects"`
 	ReconnectWait  int    `json:"reconnect_wait" yaml:"reconnect_wait"`
 	ConnectTimeout int    `json:"connect_timeout" yaml:"connect_timeout"`
-	// EncryptionKey: 16, 24, 或 32 位字符串分别对应 AES-128, 192, 256
+
 	EncryptionKey string `json:"encryption_key" yaml:"encryption_key"`
+
+	ThemeKeys map[string]string `json:"theme_keys" yaml:"theme_keys"`
 }
 
 func (c *NATSConfig) setDefaults() {
@@ -46,10 +48,11 @@ func (c *NATSConfig) setDefaults() {
 }
 
 type NyaNATS struct {
-	err      error
-	natsConn *nats.Conn
-	debug    *log.Logger
-	key      []byte // 内部存储字节类型的密钥
+	err        error
+	natsConn   *nats.Conn
+	debug      *log.Logger
+	defaultKey []byte
+	themeKeys  map[string][]byte
 }
 
 func (p *NyaNATS) logf(format string, v ...interface{}) {
@@ -58,17 +61,23 @@ func (p *NyaNATS) logf(format string, v ...interface{}) {
 	}
 }
 
-// encrypt 使用 AES-GCM 加密数据
-func (p *NyaNATS) encrypt(plaintext []byte) ([]byte, error) {
-	if len(p.key) == 0 {
+func (p *NyaNATS) getKey(theme string) []byte {
+	if key, ok := p.themeKeys[theme]; ok {
+		return key
+	}
+	return p.defaultKey
+}
+
+func (p *NyaNATS) encrypt(theme string, plaintext []byte) ([]byte, error) {
+	key := p.getKey(theme)
+	if len(key) == 0 {
 		return plaintext, nil
 	}
 
-	block, err := aes.NewCipher(p.key)
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
@@ -79,21 +88,19 @@ func (p *NyaNATS) encrypt(plaintext []byte) ([]byte, error) {
 		return nil, err
 	}
 
-	// 封口：将加密结果追加在 nonce 后面
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
-// decrypt 使用 AES-GCM 解密数据
-func (p *NyaNATS) decrypt(ciphertext []byte) ([]byte, error) {
-	if len(p.key) == 0 {
+func (p *NyaNATS) decrypt(theme string, ciphertext []byte) ([]byte, error) {
+	key := p.getKey(theme)
+	if len(key) == 0 {
 		return ciphertext, nil
 	}
 
-	block, err := aes.NewCipher(p.key)
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
@@ -101,7 +108,7 @@ func (p *NyaNATS) decrypt(ciphertext []byte) ([]byte, error) {
 
 	nonceSize := gcm.NonceSize()
 	if len(ciphertext) < nonceSize {
-		return nil, fmt.Errorf("ciphertext too short")
+		return nil, fmt.Errorf("E: CIPHER SHORT")
 	}
 
 	nonce, actualCiphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
@@ -121,25 +128,32 @@ func New(configString string, debug *log.Logger) *NyaNATS {
 
 func NewC(config NATSConfig, debug *log.Logger) *NyaNATS {
 	config.setDefaults()
-	p := &NyaNATS{debug: debug}
-
-	if config.EncryptionKey != "" {
-		p.key = []byte(config.EncryptionKey)
-		// 校验 AES 密钥长度
-		if l := len(p.key); l != 16 && l != 24 && l != 32 {
-			p.err = fmt.Errorf("E: AES KEY LEN (%d), MUST BE 16, 24, OR 32", l)
-			return p
-		}
-		p.logf("MODE: ENCRYPTED")
+	p := &NyaNATS{
+		debug:     debug,
+		themeKeys: make(map[string][]byte),
 	}
 
-	var url string
+	if config.EncryptionKey != "" {
+		p.defaultKey = []byte(config.EncryptionKey)
+		if l := len(p.defaultKey); l != 16 && l != 24 && l != 32 {
+			p.err = fmt.Errorf("E: DEFAULT KEY LEN %d", l)
+			return p
+		}
+	}
+
+	for theme, kStr := range config.ThemeKeys {
+		kByte := []byte(kStr)
+		if l := len(kByte); l != 16 && l != 24 && l != 32 {
+			p.err = fmt.Errorf("E: THEME [%s] KEY LEN %d", theme, l)
+			return p
+		}
+		p.themeKeys[theme] = kByte
+		p.logf("THEME KEY LOADED: %s", theme)
+	}
+
+	url := fmt.Sprintf("nats://%s", config.NatsServer)
 	if config.NatsUser != "" {
-		url = fmt.Sprintf("nats:/%s:%s@%s", config.NatsUser, config.NatsPassword, config.NatsServer)
-		p.logf("CONN %s@%s", config.NatsUser, config.NatsServer)
-	} else {
-		url = fmt.Sprintf("nats:/%s", config.NatsServer)
-		p.logf("CONN %s", config.NatsServer)
+		url = fmt.Sprintf("nats://%s:%s@%s", config.NatsUser, config.NatsPassword, config.NatsServer)
 	}
 
 	opts := []nats.Option{
@@ -154,7 +168,6 @@ func NewC(config NATSConfig, debug *log.Logger) *NyaNATS {
 
 	nc, err := nats.Connect(url, opts...)
 	if err != nil {
-		p.logf("E CON: %v", err)
 		p.err = err
 		return p
 	}
@@ -172,24 +185,21 @@ func (p *NyaNATS) Subscribe(theme string, callback func(m string) string) error 
 	_, err := p.natsConn.Subscribe(theme, func(m *nats.Msg) {
 		p.logf("<- %s , LEN: %d", theme, len(m.Data))
 
-		// 解密输入
-		data, err := p.decrypt(m.Data)
+		data, err := p.decrypt(m.Subject, m.Data)
 		if err != nil {
-			p.logf("E DECRYPT: %v", err)
+			p.logf("E DECRYPT [%s]: %v", m.Subject, err)
 			return
 		}
 
 		replyContent := callback(string(data))
 
 		if m.Reply != "" {
-			// 加密响应
-			encryptedReply, err := p.encrypt([]byte(replyContent))
+
+			encryptedReply, err := p.encrypt(m.Subject, []byte(replyContent))
 			if err != nil {
 				p.logf("E ENC-RESP: %v", err)
 				return
 			}
-
-			p.logf("-> %s , LEN: %d", m.Reply, len(encryptedReply))
 			if err := m.Respond(encryptedReply); err != nil {
 				p.logf("E SND: %s : %v", m.Reply, err)
 			}
@@ -208,17 +218,15 @@ func (p *NyaNATS) Publish(theme string, message string) error {
 	if p.err != nil {
 		return p.err
 	}
-
-	data, err := p.encrypt([]byte(message))
+	data, err := p.encrypt(theme, []byte(message))
 	if err != nil {
 		return err
 	}
-
 	err = p.natsConn.Publish(theme, data)
 	if err != nil {
 		p.logf("E PUB: %s : %v", theme, err)
 	} else {
-		p.logf("-> %s (ENC:%v)", theme, len(p.key) > 0)
+		p.logf("-> %s", theme)
 	}
 	return err
 }
@@ -227,8 +235,7 @@ func (p *NyaNATS) Request(theme string, message string, timeout time.Duration) (
 	if p.err != nil {
 		return "", p.err
 	}
-
-	data, err := p.encrypt([]byte(message))
+	data, err := p.encrypt(theme, []byte(message))
 	if err != nil {
 		return "", err
 	}
@@ -240,13 +247,11 @@ func (p *NyaNATS) Request(theme string, message string, timeout time.Duration) (
 		return "", err
 	}
 
-	// 解密返回的消息
-	decryptedData, err := p.decrypt(msg.Data)
+	decryptedData, err := p.decrypt(theme, msg.Data)
 	if err != nil {
-		p.logf("E DECRYPT-RES: %v", err)
+		p.logf("E DECRYPT-RES [%s]: %v", theme, err)
 		return "", err
 	}
-
 	p.logf("<- %s", theme)
 	return string(decryptedData), nil
 }
@@ -258,6 +263,4 @@ func (p *NyaNATS) Close() {
 	}
 }
 
-func (p *NyaNATS) Error() error {
-	return p.err
-}
+func (p *NyaNATS) Error() error { return p.err }
